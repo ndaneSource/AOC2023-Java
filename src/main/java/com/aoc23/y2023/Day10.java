b2a92d4e7cf3df08291cdbf9e8cb66e02f4371fe
natedane <natedane@gmail.com>
2023-12-11 20:53:30 -0700

    finished day 10

--- src/main/java/com/aoc23/y2023/Day10.java
+++ src/main/java/com/aoc23/y2023/Day10.java
@@ -1,12 +1,22 @@
 package com.aoc23.y2023;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+
+import org.javatuples.Pair;
 
 import com.aoc23.util.IDay;
 
+import io.reactivex.rxjava3.internal.fuseable.QueueDisposable;
+
 public class Day10 extends IDay{
     
+    HashSet<String> leftSide = new HashSet<String>();
+    HashSet<String> rightSide = new HashSet<String>();
     public Day10() {
         super("10");
     }
@@ -16,10 +26,21 @@ public class Day10 extends IDay{
 
         ArrayList<String> loop = traverse(input, start, new ArrayList<String>(), "");
         //System.out.println(loop.toString());
-        return loop.size()/2;
+        return Math.floorDiv(loop.size(), 2);
     }
 
     public int solution2(List<String> input){
+        int[] start = findS(input);
+        ArrayList<String> loop = traverse(input, start, new ArrayList<String>(), "");
+
+        System.out.println((1)+","+58+"contained? "+leftSide.contains((1)+","+58));
+
+        rightSide.removeAll(loop);
+        leftSide.removeAll(loop);
+        System.out.println((1)+","+58+"contained? "+leftSide.contains((1)+","+58));
+
+        visualize(input, loop);
+        //fillSpace(input);
         return 0;
     }
 
@@ -30,122 +51,261 @@ public class Day10 extends IDay{
         }
         return null;
     }
+
     public ArrayList<String> traverse(List<String> input, int[]position, ArrayList<String> path, String direction){
         int lineNumber = position[0];
         int posNumber = position[1];
+
         while(true){
         
         if(input.get(lineNumber).substring(posNumber, posNumber+1).equals("S") && path.size()>0)
                 return path;
 
-        path.add(lineNumber+","+posNumber);
+        String pos = lineNumber+","+posNumber;
+        path.add(pos);
+        leftSide.remove(pos);
+        rightSide.remove(pos);
         switch(input.get(lineNumber).substring(posNumber, posNumber+1))
         {
             case "-":
                 if(direction.equals("L")){
-                        posNumber++;
-                        direction = "L";
-                    }
-                else{
-                    int[] nextPos = new int[]{lineNumber, posNumber-1};
-                    ArrayList<String> p = traverse(input, nextPos, path, "R");
+                    leftSide.add((lineNumber-1)+","+posNumber);
+                    rightSide.add((lineNumber+1)+","+posNumber);
+                    posNumber++;
+                    direction = "L";
                     
-                    if(p!= null)
-                        return p;
+                }
+                else{
+                    leftSide.add((lineNumber+1)+","+posNumber);
+                    rightSide.add((lineNumber-1)+","+posNumber);
+                    posNumber--;
+                    direction = "R";
+                    
                 }
                 break;
             case "|":
                 if(direction.equals("U")){
-                    int[] nextPos = new int[]{lineNumber+1, posNumber+0};
-                    ArrayList<String> p = traverse(input, nextPos, path, "U");
-                    if(p!= null)
-                        return p;
+                    leftSide.add((lineNumber)+","+(posNumber+1));
+                    rightSide.add((lineNumber)+","+(posNumber-1));
+                    lineNumber++;
+                    direction = "U";
+                    
                 }
                 else{
-                    int[] nextPos = new int[]{lineNumber-1, posNumber-0};
-                    ArrayList<String> p = traverse(input, nextPos, path, "D");
-                    if(p!= null)
-                        return p;
+                    leftSide.add((lineNumber)+","+(posNumber-1));
+                    rightSide.add((lineNumber)+","+(posNumber+1));
+                    direction = "D";
+                    lineNumber--;
+                    
                 }
                 break;
             case "J":
                 if(direction.equals("U")){
-                        int[] nextPos = new int[]{lineNumber-0, posNumber-1};
-                        ArrayList<String> p = traverse(input, nextPos, path, "R");
-                        if(p!= null)
-                            return p;
-                    }
+                    leftSide.add((lineNumber+1)+","+(posNumber));
+                    leftSide.add((lineNumber)+","+(posNumber+1));
+                    posNumber--;
+                    direction = "R";
+                    
+                }
                 else{
-                        int[] nextPos = new int[]{lineNumber-1, posNumber-0};
-                        ArrayList<String> p = traverse(input, nextPos, path, "D");
-                        if(p!= null)
-                            return p;
-                    }
+                    direction = "D";
+                    rightSide.add((lineNumber+1)+","+(posNumber));
+                    rightSide.add((lineNumber)+","+(posNumber+1));
+                    lineNumber--;
+                    
+                }
                 break;
             case "L":
                 if(direction.equals("U")){
-                        int[] nextPos = new int[]{lineNumber+0, posNumber+1};
-                        ArrayList<String> p = traverse(input, nextPos, path, "L");
-                        if(p!= null)
-                            return p;
-                    }
+                    rightSide.add((lineNumber+1)+","+(posNumber));
+                    rightSide.add((lineNumber)+","+(posNumber-1));
+                    posNumber++;
+                    direction = "L";
+                    
+                }
                 else {
-                        int[] nextPos = new int[]{lineNumber-1, posNumber-0};
-                        ArrayList<String> p = traverse(input, nextPos, path, "D");
-                        if(p!= null)
-                            return p;
-                    }
+                    direction = "D";
+                    leftSide.add((lineNumber+1)+","+(posNumber));
+                    leftSide.add((lineNumber)+","+(posNumber-1));
+                    lineNumber--;
+                    
+                }
                 break;
             case "F":
                 if(direction.equals("D")){
-                        int[] nextPos = new int[]{lineNumber+0, posNumber+1};
-                        ArrayList<String> p = traverse(input, nextPos, path, "L");  
-                        if(p!= null)
-                            return p;
-                    }
+                    leftSide.add((lineNumber-1)+","+(posNumber));
+                    leftSide.add((lineNumber)+","+(posNumber-1));
+                    posNumber++;
+                    direction = "L";
+                    if(lineNumber == 2)
+                        System.out.println("here");
+                    if(lineNumber == 2)
+                        System.out.println((lineNumber-1)+","+posNumber+"contained? "+leftSide.contains((lineNumber-1)+","+posNumber));
+                }
                 else {
-                       int[] nextPos = new int[]{lineNumber+1, posNumber+0};
-                        ArrayList<String> p = traverse(input, nextPos, path, "U");
-                        if(p!= null)
-                            return p;
-                    }
+                    rightSide.add((lineNumber-1)+","+(posNumber));
+                    rightSide.add((lineNumber)+","+(posNumber-1));
+                    lineNumber++;
+                    direction = "U";
+                    
+                }
                 break;
             case "7":
                 //left and down
                 if(direction.equals("D")){
-                        int[] nextPos = new int[]{lineNumber-0, posNumber-1};
-                        ArrayList<String> p = traverse(input, nextPos, path, "R");
-                        if(p!= null)
-                            return p;
-                    }
+                    rightSide.add((lineNumber-1)+","+(posNumber));
+                    rightSide.add((lineNumber)+","+(posNumber+1));
+                    posNumber--;
+                    direction = "R";
+                    
+                }
                 else {
-                        int[] nextPos = new int[]{lineNumber+1, posNumber+0};
-                        path = traverse(input, nextPos, path, "U");
-                        break;
-                    }
+                    leftSide.add((lineNumber-1)+","+(posNumber));
+                    leftSide.add((lineNumber)+","+(posNumber+1));
+                    lineNumber++;
+                    direction = "U";
+                    
+                }
                 break;
             case "S":
-                int[] nextPos = new int[]{lineNumber+0, posNumber-1};
-                return traverse(input, nextPos, path,"R");
+                //test
+                // rightSide.add((lineNumber-1)+","+(posNumber));
+                // rightSide.add((lineNumber)+","+(posNumber-1)); 
+                // posNumber++;
+                // direction = "L";
                 
+
+                //input
+                leftSide.add((lineNumber+1)+","+(posNumber));
+                leftSide.add((lineNumber)+","+(posNumber+1));
+                posNumber--;
+                direction = "R";
+                
+                break;
             default:
                 return path;
         }
     }
-        return path;
+        //return path;
     }
 
-    // private boolean canSearch(int pos, int lineNumber, List<String> input) {
-    //     if(pos <0 || pos >= input.get(0).length())
-    //         return false;
+    public void visualize(List<String> input, ArrayList<String>path){
+        for(int i =0; i < input.size();i++){
+            StringBuilder sb = new StringBuilder(input.get(i));
 
-    //     if(lineNumber <0 || pos >= input.size())
-    //         return false;
-        
-    //     // if(path.contains(lineNumber+","+pos))
-    //     //     return false;
+            boolean firstX = false;
+            int lastX = -1;
 
-    //     return true;
-    // }
+            for(int j=0; j< input.get(i).length();j++){
+                char test = sb.charAt(j);
+                if(path.contains(i+","+j)){
+                    //sb.setCharAt(j, 'X');
+                    firstX = true;
+                    lastX = j;
+                }
+                else if(leftSide.contains(i+","+j)){
+                    sb.setCharAt(j, 'Z');
+                }
+                else if(rightSide.contains(i+","+j)){
+                    sb.setCharAt(j, 'R');
+                }
+                else if(!firstX)
+                    sb.setCharAt(j, 'O');
+                else
+                    sb.setCharAt(j, ' ');
 
+
+
+            }
+            if(lastX>0){
+                int trailing = sb.length() - lastX-2;
+                sb.replace(lastX+2, sb.length(), "O");
+                sb.repeat("O", trailing);
+            }
+            input.set(i, sb.toString());
+            if(i > 0)
+              input = findOutside(input,i);
+        }
+
+        input.stream().forEach(System.out::println);
+    }
+
+
+    public List<String> findOutside(List<String> input, int line){
+        Set<Pair<Integer,Integer>> searched = new HashSet<Pair<Integer,Integer>>();
+        Queue<Pair<Integer,Integer>> searching= new PriorityQueue<Pair<Integer,Integer>>();
+
+        StringBuilder sbLine = new StringBuilder(input.get(line));
+        StringBuilder sbLinePrev = new StringBuilder(input.get(line-1));
+
+        for(int i=0; i < sbLine.length(); i++){
+            if(sbLine.charAt(i) == ' '){
+                int maxLength = input.get(line-1).length()-1;
+
+                for(int j =-1; j<2; j++)
+                    if(i+j > 0 && i+j < maxLength){
+                        if(sbLinePrev.charAt(i+j) == 'O'){
+                            sbLine.setCharAt(i, 'O');
+                            searching.add(new Pair<Integer,Integer>(line-1, i+j));
+                            searching.add(new Pair<Integer,Integer>(line, i+j));  
+                        }            
+                    }
+            }
+            if(sbLine.charAt(i) == 'O'){
+                int maxLength = input.get(line-1).length()-1;
+
+                for(int j =-1; j<2; j++)
+                    if(i+j > 0 && i+j < maxLength){
+                            searching.add(new Pair<Integer,Integer>(line-1, i+j));
+                            searching.add(new Pair<Integer,Integer>(line, i+j));  
+                    }
+            }
+        }
+        input.set(line, sbLine.toString());
+        input.set(line-1, sbLinePrev.toString());
+
+        while(searching.size()>0){
+            Pair<Integer,Integer> pos = searching.poll();
+            
+            if(searched.contains(pos))
+                continue;
+            StringBuilder sb = new StringBuilder(input.get(pos.getValue0()));
+
+            if(pos.getValue0() == 1 && pos.getValue1() == 58){
+                System.out.println((1)+","+58+"contained??? "+leftSide.contains((1)+","+58) );
+                System.out.println(sb.charAt(pos.getValue1()));
+            }
+            
+
+            searched.add(pos);
+            if(sb.charAt(pos.getValue1()) == ' '){
+                sb.setCharAt(pos.getValue1(), 'O');
+                input.set(pos.getValue0(), sb.toString());
+                for(int x =-1; x<2; x++)
+                    for(int y =-1; y<2; y++)
+                        if(pos.getValue0()+x >0 && pos.getValue0()+x < input.size()-1 
+                        && pos.getValue1()+y > 0 && pos.getValue1()+y <input.get(pos.getValue0()+x).length()-1)
+                            searching.add(new Pair<Integer,Integer>(pos.getValue0()+x, pos.getValue1()+y));    
+            }
+        }
+        //less 684, more 380
+
+        //150R
+        return input;
+    }
+
+
+    public void fillSpace(List<String> input){
+        int count = 0;
+        for(int i =0; i < input.size(); i++){
+            StringBuilder sbLine = new StringBuilder(input.get(i));
+            for(int j=0; j < sbLine.length(); j++){
+                if(sbLine.charAt(j)=='R')
+                    count++;
+
+                    
+            }
+
+        }
+    }
 }

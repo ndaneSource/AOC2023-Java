6140b219cdf3d436e728bf87741382c208165a01
natedane <natedane@gmail.com>
2023-12-17 12:06:13 -0700

    starting day17

---
+++ src/main/java/com/aoc23/y2023/Day17.java
@@ -0,0 +1,24 @@
+package com.aoc23.y2023;
+
+import java.util.List;
+import java.util.Map;
+
+import com.aoc23.util.IDay;
+
+public class Day17 extends IDay{
+    
+    public Map<String, Integer> cubeMap;
+
+    public Day17() {
+        super("17");
+    }
+
+    public int solution1(List<String> input){
+        return 0;
+    }
+
+    public int solution2(List<String> input){
+        return 0;
+    }
+
+}

65a627057287cafec4291587ac01a257268f03da
natedane <natedane@gmail.com>
2023-12-17 12:03:31 -0700

    finish day 16 2

--- src/main/java/com/aoc23/y2023/Day16.java
+++ src/main/java/com/aoc23/y2023/Day16.java
@@ -3,7 +3,6 @@ package com.aoc23.y2023;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;

1c1d62c41bdbb4cf576a28dd124298ffb6ac3da1
natedane <natedane@gmail.com>
2023-12-20 07:25:01 -0700

    starting 20, init 19

--- src/main/java/com/aoc23/Main.java
+++ src/main/java/com/aoc23/Main.java
@@ -15,7 +15,7 @@ public class Main {
     }
 
     public static void runSolution(int part, String fileName){
-        IDay run = new Day18();
+        IDay run = new Day20();
         List<String> input = run.getFileList(fileName);
         int solution = 0;
         if(part == 1)
---
+++ src/main/java/com/aoc23/y2023/Day20.java
@@ -0,0 +1,24 @@
+package com.aoc23.y2023;
+
+import java.util.List;
+import java.util.Map;
+
+import com.aoc23.util.IDay;
+
+public class Day20 extends IDay{
+    
+    public Map<String, Integer> cubeMap;
+
+    public Day20() {
+        super("20");
+    }
+
+    public int solution1(List<String> input){
+        return 0;
+    }
+
+    public int solution2(List<String> input){
+        return 0;
+    }
+
+}
